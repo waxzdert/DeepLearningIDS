806a4a9c124e2f466f48c7bc06357c14a4372527
WU,DONG-YE <waxzdert16@gmail.com>
2018-03-22 22:37:56 +0800

    Data Preprocessing v0.2

--- RNN/DataPreprocessing.py
+++ RNN/DataPreprocessing.py
@@ -1,15 +1,14 @@
 import tensorflow as tf
 import pandas as pd
+import math
 
 file = 'C:\\Users\\MaxWu\\Documents\\GitHub\\DeepLearningIDS\\Datasets\\KDDTrain+_Raw.csv'
-
 raw_data = pd.read_csv(file)
 
 #計算資料的特徵中有多少獨立項
 #print(raw_data.groupby('protocol_type').ngroups)
 #print(raw_data.groupby('Service').ngroups)
 #print(raw_data.groupby('flag').ngroups)
-
 
 #================== OneHot Encoding ==================
 def one_hot(in_df):
@@ -23,17 +22,32 @@ def one_hot(in_df):
     oh_df = pd.get_dummies(data=oh_df, columns=['Service'])
     #將flag轉換為數值, dim: 112->122,(125973, 122)
     oh_df = pd.get_dummies(data=oh_df, columns=['flag'])
-    
     return oh_df
 
 #================== Normalization ==================
 def fir_norm(in_df):
-    
     # Normalize the data which difference between maximum and minimum values
     # such as : ‘duration[0,58329]’,‘src_bytes[0,1.3 × 109]’ and ‘dst_bytes
-    # Logistic method: new Xi = ((old Xi)-min)/(Max-min)
+    # Logarithmic scaling method for scaling to obtain the ranges
+    # (x,y) -> (log(x),log(y))
+    for i in range(len(in_df['Duartion'])):
+        if (in_df.loc[i,('Duration')]) == 0:
+            in_df.loc[i,('Duration')] = 0
+        else:
+            in_df.loc[i,('Duration')] = round(math.log(in_df.loc[i,('Duration')],10), 2)
 
-    
+    for i in range(len(in_df['src_bytes'])):
+        if (in_df.loc[i,('src_bytes')]) == 0:
+            in_df.loc[i,('src_bytes')] = 0
+        else:
+            in_df.loc[i,('src_bytes')] = round(math.log(in_df.loc[i,('src_bytes')],10), 2)
+
+    for i in range(len(in_df['dst_bytes'])):
+        if (in_df.loc[i,('dst_bytes')]) == 0:
+            in_df.loc[i,('dst_bytes')] = 0
+        else:
+            in_df.loc[i,('dst_bytes')] = round(math.log(in_df.loc[i,('dst_bytes')],10), 2)
 
 def sec_norm(in_df)
-
+    #
+    # new Xi = ((old Xi)-min)/(Max-min)
